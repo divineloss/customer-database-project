608329f0d72205378ab2ef6304387f966ea7f1b0
Elizabeth Kalaj <lskalaj12@gmail.com>
2022-07-14 08:43:36 -0700

    adding web-scraping folder + firstnames web-scraping file

---
+++ web-scraping/firstnames.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.verywellfamily.com/top-1000-baby-boy-names-2757618"
+req = requests.get(url)
+data = BeautifulSoup(req.text, features="html.parser")
+ol = data.find("ol")
+file = open("firstnames", "w")
+for ol in ol.find_all("li"):
+    file.write(f"{ol.text}\n")
+file.close()    
